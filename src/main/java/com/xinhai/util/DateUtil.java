02249f3877c81421c012faf109cbeefe1b0e64c7
黄官易 <hgy@x.xinhai.com>
2018-05-29 10:53:42 +0800

    新增 时间工具类 判断一个时间 是否是本天 本周 本月的方法

--- src/main/java/com/xinhai/util/DateUtil.java
+++ src/main/java/com/xinhai/util/DateUtil.java
@@ -260,6 +260,39 @@ public final class DateUtil {
 		return result.toString();
 	}
 
+	// 判断选择的日期是否是本周
+	public static boolean isThisWeek(long time) {
+		Calendar calendar = Calendar.getInstance();
+		int currentWeek = calendar.get(Calendar.WEEK_OF_YEAR);
+		calendar.setTime(new Date(time));
+		int paramWeek = calendar.get(Calendar.WEEK_OF_YEAR);
+		if (paramWeek == currentWeek) {
+			return true;
+		}
+		return false;
+	}
+
+	// 判断选择的日期是否是今天
+	public static boolean isToday(long time) {
+		return isThisTime(time, "yyyy-MM-dd");
+	}
+
+	// 判断选择的日期是否是本月
+	public static boolean isThisMonth(long time) {
+		return isThisTime(time, "yyyy-MM");
+	}
+
+	private static boolean isThisTime(long time, String pattern) {
+		Date date = new Date(time);
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		String param = sdf.format(date);// 参数时间
+		String now = sdf.format(new Date());// 当前时间
+		if (param.equals(now)) {
+			return true;
+		}
+		return false;
+	}
+
 	// /**
 	// *
 	// * @Title: addMonth
