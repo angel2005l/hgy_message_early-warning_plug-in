fefeea0b571b6d4e7e14b0b3c567008dc39a5713
黄官易 <hgy@x.xinhai.com>
2018-05-29 12:38:14 +0800

    优化 优化微信基础路径配置

--- src/main/java/com/xinhai/util/WeiXinUtil.java
+++ src/main/java/com/xinhai/util/WeiXinUtil.java
@@ -3,23 +3,28 @@ package com.xinhai.util;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.xinhai.entity.Warning;
 
 public class WeiXinUtil {
-	// private static final Logger log = LoggerFactory.getLogger(WeiXinUtil.class);
-	// String url =
 	// "http://10.168.20.223:8088/ddapi/wx/templatemes/warn7.jsp?title=1&message=mes&create_time=2018&openid=oVM1v1LYIa38thd9xm6MR0-AoVb0&go_url=http://www.baidu.com";
+	private static final Logger log = LoggerFactory.getLogger(WeiXinUtil.class);
 
-	public static void sendWarning(Warning obj, String openId, String url) throws UnsupportedEncodingException {
-		if (StrUtil.isBlank(url)) {
-			url = "http://10.168.20.223:8090/ddapi/wx/templatemes/warn7.jsp";
+	public static void sendWarning(Warning obj, String openId) throws UnsupportedEncodingException {
+		ResourceBundle rb = ResourceBundle.getBundle("dataDic");
+		String url = rb.getString("weixin_push_path");
+		String rootPath = rb.getString("root_path");
+		if (StrUtil.isBlank(url) || StrUtil.isBlank(rootPath)) {
+			log.info("微信推送接口缺少基础地址配置");
+			return;
 		}
-		StringBuffer goUrl = new StringBuffer("192.168.0.100:8088/xh_f7_sprider/myData?warnId=").append(obj.getGuid())
-				.append("&method=mod4");
+		StringBuffer goUrl = new StringBuffer().append("/myData?warnId=").append(obj.getGuid()).append("&method=mod4");
 		List<NameValuePair> data = new ArrayList<NameValuePair>();
 		data.add(new BasicNameValuePair("title", new String(obj.getTitle().getBytes(), "utf-8")));
 		data.add(new BasicNameValuePair("message", obj.getMessage()));
@@ -29,15 +34,4 @@ public class WeiXinUtil {
 		data.add(new BasicNameValuePair("go_url", goUrl.toString()));
 		HttpClientUtil.getPostDefault(url, data);
 	}
-
-	// public static void main(String[] arg) throws SQLException,
-	// UnsupportedEncodingException {
-	// IDataCollectDao dao = new DataCollectDaoImpl();
-	// List<Warning> selHandleData = dao.selHandleData(1);
-	// System.err.println(selHandleData.size());
-	// Warning warning = selHandleData.get(0);
-	//
-	// WeiXinUtil.sendWarning(warning, "oVM1v1LYIa38thd9xm6MR0-AoVb0", "");
-	//
-	// }
 }
